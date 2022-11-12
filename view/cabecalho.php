d2169c84be15117fb76a752144dd2b845fc75bec
= <=>
2022-11-12 15:33:01 -0300

    cabecalho style ini

--- view/cabecalho.php
+++ view/cabecalho.php
@@ -1,1 +1,12 @@
-<div class="logo"><h1>Logo</h1></div>
+<div class="cabecalho-content">
+    <div class="logo">
+        <h1>Logo</h1>
+    </div>
+
+    <div class="profile-parent">
+        <div class="profile-children header">Profile</div>
+        <div class="profile-item-children itens">
+            <a href="logout.php" class="item"><span>Logout</span></a>
+        </div>
+    </div>
+</div>
