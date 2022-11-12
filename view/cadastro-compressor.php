0a3cffcbb391b645fc6c5387572b3fc5fd700be5
= <=>
2022-11-11 21:34:36 -0300

    update files

--- view/cadastro-compressor.php
+++ view/cadastro-compressor.php
@@ -50,13 +50,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="">Daelly Confec��es</a>
+                <a class="navbar-brand" href="">Daelly Confecções</a>
                 <ul class="user-menu">
                     <li class="dropdown pull-right">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <svg class="glyph stroked male-user">
                                 <use xlink:href="#stroked-male-user"></use>
-                            </svg><span class="nome_usuario">Usu�rio Logado </span><span class="caret"></span>
+                            </svg><span class="nome_usuario">Usuário Logado </span><span class="caret"></span>
                         </a>
                         <ul class="dropdown-menu" role="menu">
                             <li><a href="logout.php"><svg class="glyph stroked cancel">
