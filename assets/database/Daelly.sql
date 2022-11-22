5d15f2e01ff120a192102df7d6271605c3b4eab5
Davi3234 <74547139+Davi3234@users.noreply.github.com>
2022-11-22 13:18:34 -0300

    update files

--- assets/database/Daelly.sql
+++ assets/database/Daelly.sql
@@ -1,111 +1,310 @@
-# Create Database
-
-DROP DATABASE `daelly`;
-CREATE DATABASE `daelly`;
+CREATE DATABASE  IF NOT EXISTS `daelly` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `daelly`;
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: daelly
+-- ------------------------------------------------------
+-- Server version	5.5.16
 
-# Create Tables
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `administrador`
+--
 
 DROP TABLE IF EXISTS `administrador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `administrador` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(50) DEFAULT NULL,
-  `email` VARCHAR(50) DEFAULT NULL,
-  `senha` VARCHAR(50) DEFAULT NULL,
-  `tentativas` INT DEFAULT NULL,
-  `ultimo_acesso` DATETIME DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `senha` varchar(50) DEFAULT NULL,
+  `tentativas` int(11) DEFAULT NULL,
+  `ultimo_acesso` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-);
-INSERT INTO `administrador` (`nome`, `email`, `senha`, `tentativas`)  VALUES ('Admin', 'admin@admin.gmail.com', '21232f297a57a5a743894a0e4a801fc3', 0);
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `grupo`;
-CREATE TABLE `grupo` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `numero` INT NOT NULL,
-  PRIMARY KEY (`id`)
-);
+--
+-- Dumping data for table `administrador`
+--
 
-DROP TABLE IF EXISTS `funcionario`;
-CREATE TABLE `funcionario` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `id_grupo` INT DEFAULT NULL,
-  `cpf` VARCHAR(14) NOT NULL,
-  `nome` VARCHAR(50) NULL,
-  `entrada` VARCHAR(10) NULL,
-  `saida` VARCHAR(10) NULL,
-  PRIMARY KEY (`id`)
-);
+LOCK TABLES `administrador` WRITE;
+/*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
+INSERT INTO `administrador` VALUES (1,'Admin','admin@admin.gmail.com','21232f297a57a5a743894a0e4a801fc3',0,NULL);
+/*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
+UNLOCK TABLES;
 
-DROP TABLE IF EXISTS `funcao`;
-CREATE TABLE `funcao` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `id_tipo` INT DEFAULT NULL,
-  `nome` VARCHAR(50) NOT NULL,
+--
+-- Table structure for table `compressor`
+--
+
+DROP TABLE IF EXISTS `compressor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compressor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `codigo` int(11) NOT NULL,
+  `marca` varchar(50) DEFAULT NULL,
+  `modelo` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compressor`
+--
+
+LOCK TABLES `compressor` WRITE;
+/*!40000 ALTER TABLE `compressor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compressor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funca_funci`
+--
 
 DROP TABLE IF EXISTS `funca_funci`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `funca_funci` (
-  `id_funcionario` INT NOT NULL,
-  `id_funcao` INT NOT NULL,
+  `id_funcionario` int(11) NOT NULL,
+  `id_funcao` int(11) NOT NULL,
   PRIMARY KEY (`id_funcionario`,`id_funcao`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funca_funci`
+--
+
+LOCK TABLES `funca_funci` WRITE;
+/*!40000 ALTER TABLE `funca_funci` DISABLE KEYS */;
+/*!40000 ALTER TABLE `funca_funci` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funcao`
+--
+
+DROP TABLE IF EXISTS `funcao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funcao` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tipo` int(11) DEFAULT NULL,
+  `nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funcao`
+--
+
+LOCK TABLES `funcao` WRITE;
+/*!40000 ALTER TABLE `funcao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `funcao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funcionario`
+--
+
+DROP TABLE IF EXISTS `funcionario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funcionario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_grupo` int(11) DEFAULT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `entrada` varchar(10) DEFAULT NULL,
+  `saida` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funcionario`
+--
+
+LOCK TABLES `funcionario` WRITE;
+/*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
+INSERT INTO `funcionario` VALUES (1,NULL,'123.312.312-31','aad','1231-03-12',''),(2,NULL,'123.124.215-15','sgsdhgh','1111-12-12','');
+/*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grupo`
+--
+
+DROP TABLE IF EXISTS `grupo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `grupo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `numero` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grupo`
+--
+
+LOCK TABLES `grupo` WRITE;
+/*!40000 ALTER TABLE `grupo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grupo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manutencao`
+--
+
+DROP TABLE IF EXISTS `manutencao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `manutencao` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_maquina_costura` int(11) DEFAULT NULL,
+  `id_compressor` int(11) DEFAULT NULL,
+  `descricao` varchar(150) DEFAULT NULL,
+  `data_manutencao` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manutencao`
+--
+
+LOCK TABLES `manutencao` WRITE;
+/*!40000 ALTER TABLE `manutencao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `manutencao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mapa`
+--
+
+DROP TABLE IF EXISTS `mapa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mapa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `largura_mapa` int(11) NOT NULL,
+  `altura_mapa` int(11) NOT NULL,
+  `largura_mc` int(11) NOT NULL,
+  `altura_mc` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mapa`
+--
+
+LOCK TABLES `mapa` WRITE;
+/*!40000 ALTER TABLE `mapa` DISABLE KEYS */;
+INSERT INTO `mapa` VALUES (1,1461,3000,75,75);
+/*!40000 ALTER TABLE `mapa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `maquina_costura`
+--
+
+DROP TABLE IF EXISTS `maquina_costura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `maquina_costura` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tipo` int(11) NOT NULL,
+  `codigo` int(11) NOT NULL,
+  `modelo` varchar(50) DEFAULT NULL,
+  `marca` varchar(50) DEFAULT NULL,
+  `chassi` varchar(50) DEFAULT NULL,
+  `aquisicao` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `maquina_costura`
+--
+
+LOCK TABLES `maquina_costura` WRITE;
+/*!40000 ALTER TABLE `maquina_costura` DISABLE KEYS */;
+/*!40000 ALTER TABLE `maquina_costura` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `maquina_costura_mapa`
+--
+
+DROP TABLE IF EXISTS `maquina_costura_mapa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `maquina_costura_mapa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_maquina_costura` int(11) NOT NULL,
+  `posicionado` int(11) DEFAULT '0',
+  `x` int(11) DEFAULT '0',
+  `y` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `maquina_costura_mapa`
+--
+
+LOCK TABLES `maquina_costura_mapa` WRITE;
+/*!40000 ALTER TABLE `maquina_costura_mapa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `maquina_costura_mapa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipo`
+--
 
 DROP TABLE IF EXISTS `tipo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tipo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `maquina_costura`;
-CREATE TABLE `maquina_costura` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `id_tipo` INT NOT NULL,
-  `codigo` INT NOT NULL,
-  `modelo` VARCHAR(50) NULL,
-  `marca` VARCHAR(50) NULL,
-  `chassi` VARCHAR(50) NULL,
-  `aquisicao` DATE DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+--
+-- Dumping data for table `tipo`
+--
 
-DROP TABLE IF EXISTS `maquina_costura_mapa`;
-CREATE TABLE `maquina_costura_mapa` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `id_maquina_costura` INT NOT NULL,
-  `posicionado` INT NULL DEFAULT 0,
-  `x` INT NULL DEFAULT 0,
-  `y` INT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-);
+LOCK TABLES `tipo` WRITE;
+/*!40000 ALTER TABLE `tipo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-DROP TABLE IF EXISTS `mapa`;
-CREATE TABLE `mapa` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `largura_mapa` INT NOT NULL,
-  `altura_mapa` INT NOT NULL,
-  `largura_mc` INT NOT NULL,
-  `altura_mc` INT NOT NULL,
-  PRIMARY KEY (`id`)
-);
-INSERT INTO `mapa` (`largura_mapa`, `altura_mapa`, `largura_mc`, `altura_mc`)  VALUES (1461, 3000, 75, 75);
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-DROP TABLE IF EXISTS `manutencao`;
-CREATE TABLE `manutencao` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `id_maquina_costura` INT DEFAULT NULL,
-  `id_compressor` INT DEFAULT NULL,
-  `descricao` VARCHAR(150) NULL,
-  `data_manutencao` VARCHAR(10) NULL,
-  PRIMARY KEY (`id`)
-);
-
-DROP TABLE IF EXISTS `compressor`;
-CREATE TABLE `compressor` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `codigo` INT NOT NULL,
-  `marca` VARCHAR(50) NULL,
-  `modelo` VARCHAR(50) NULL,
-  PRIMARY KEY (`id`)
-);
+-- Dump completed on 2022-11-22 13:18:06
